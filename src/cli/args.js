8ee35932f98a3092d504a216978ad531f52e1770
rlexus90 <109463776+rlexus90@users.noreply.github.com>
2024-01-28 22:40:09 +0200

    feat: done cli task

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,11 @@
 const parseArgs = () => {
-    // Write your code here 
+	const env=process.env;
+	const envKeys=Object.keys(env).filter(key=>key.startsWith('--'));
+	if (envKeys.length===0) return console.log('nosing found');
+	envKeys.forEach(key=>{
+		const str =`${key.slice(2)}=${env.key}`;
+		console.log(str);
+	})
 };
 
 parseArgs();
