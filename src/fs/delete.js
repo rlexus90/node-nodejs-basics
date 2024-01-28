7f9388021eadbe91e3e60f6989f121b399664d32
rlexus90 <109463776+rlexus90@users.noreply.github.com>
2024-01-28 21:49:16 +0200

    feat: done delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,20 @@
+import * as fs from "node:fs/promises";
+import * as path from "path";
+import { fileURLToPath } from "url";
+
+const initPath = path.dirname(fileURLToPath(import.meta.url));
+const file = path.join(initPath, "files", "fileToRemove.txt");
+
 const remove = async () => {
-    // Write your code here 
+  fs.access(file)
+    .then(() => {
+      fs.rm(file);
+      console.log("file deleted");
+    })
+    .catch(() => {
+      const err = Error("FS operation failed");
+      console.error(err);
+    });
 };
 
 await remove();
