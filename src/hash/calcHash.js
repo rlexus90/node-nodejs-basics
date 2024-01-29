1a30d1c722b15d2052a2815257a2003f9c38f1b0
rlexus90 <109463776+rlexus90@users.noreply.github.com>
2024-01-29 19:49:01 +0200

    feat: done calcHahs.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,19 @@
+import * as fs from "fs";
+import * as path from "path";
+import { fileURLToPath } from "url";
+import crypto from "node:crypto";
+
+const initPath = path.dirname(fileURLToPath(import.meta.url));
+const filePath = path.join(initPath, "files", "fileToCalculateHashFor.txt");
+
 const calculateHash = async () => {
-    // Write your code here 
+  const hash = crypto.createHash("sha256");
+  const stream = fs.createReadStream(filePath);
+
+  stream.on("data", (data) => hash.update(data));
+  stream.on("end", () => {
+    console.log(hash.digest("hex"));
+  });
 };
 
 await calculateHash();
